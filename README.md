e0931df14232914bb2f93ad63b7ff2be785c7aa1
Brad Sickles <brad.sickles@gmail.com>
2020-07-28 16:45:39 -0400

    Initial draft

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-block-aws-network
+# block-aws-network
+
+Nullstone Block with network configured with public and private subnets across 3 AZs.
+IPv6 is enabled.
+
+## Inputs
+
+- `stack_name: string` - Stack
+- `env: string` - Environment
+
+## Outputs
+
+- `vpc_id: string` - VPC ID
+
+- `public_subnet_ids: list(string)` 
+  - IDs of all public subnets 
+  - resources in this subnet are available to the internet (assuming they have a public IP)
+
+- `private_subnet_ids: list(string)`
+  - IDs of all private subnets
+  - resources in these subnets have all egress access to internet
---
+++ network.tf
@@ -0,0 +1,26 @@
+module "network" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "2.44.0"
+
+  name = "${var.env}-${var.stack_name}"
+
+  cidr = "10.0.0.0/16"
+
+  azs             = data.aws_availability_zones.this.names
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+
+  enable_ipv6 = true
+
+  enable_nat_gateway = true
+  single_nat_gateway = true
+
+  tags = {
+    Stack       = var.stack_name
+    Environment = var.env
+  }
+
+  vpc_tags = {
+    Name = "${var.env}-${var.stack_name}"
+  }
+}
