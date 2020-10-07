be029c160c9acb6a912ec505f37f68f7aaacdc71
Brad Sickles <brad.sickles@gmail.com>
2020-10-07 14:44:31 -0400

    Fixing dns support

--- network.tf
+++ network.tf
@@ -11,7 +11,9 @@ module "network" {
   public_subnets  = var.public_subnets
   intra_subnets   = var.intra_subnets
 
-  enable_ipv6 = true
+  enable_ipv6          = true
+  enable_dns_hostnames = true
+  enable_dns_support   = true
 
   enable_nat_gateway = true
   single_nat_gateway = true
