8924ef9d07c9776935d057fce3039019339aa155
Brad Sickles <brad.sickles@gmail.com>
2022-02-22 21:42:22 -0500

    Added module manifest

--- nullstone.tf
+++ nullstone.tf
@@ -8,6 +8,7 @@ terraform {
 
 data "ns_workspace" "this" {}
 
+// Generate a random suffix to ensure uniqueness of resources
 resource "random_string" "resource_suffix" {
   length  = 5
   lower   = true
@@ -17,6 +18,7 @@ resource "random_string" "resource_suffix" {
 }
 
 locals {
-  resource_name = "${data.ns_workspace.this.block_ref}-${random_string.resource_suffix.result}"
   tags          = data.ns_workspace.this.tags
+  block_name    = data.ns_workspace.this.block_name
+  resource_name = "${data.ns_workspace.this.block_ref}-${random_string.resource_suffix.result}"
 }
